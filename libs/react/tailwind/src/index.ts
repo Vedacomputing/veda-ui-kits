5baf9ec968f56d7bd5b13936064969c8392a136a
Gustavo Cabezal <40568482+meisterveda@users.noreply.github.com>
2022-10-22 11:53:08 -0400

    Update index.ts

--- libs/react/tailwind/src/index.ts
+++ libs/react/tailwind/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/molecules/cards/default-card/default-card';
 export * from './lib/atoms/alerts/default-alert/default-alert';
 export * from './lib/atoms/buttons/default-button/default-button';
-export * from './lib/react-tailwind';
